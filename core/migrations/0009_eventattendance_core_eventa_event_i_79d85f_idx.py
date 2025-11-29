281e7c35260c5a7a502147f9ada380552d7933a9
kazay <kazayuki.0228@icloud.com>
2025-11-30 01:18:25 +0900

    Fix: Update migration dependency for 0009

--- core/migrations/0009_eventattendance_core_eventa_event_i_79d85f_idx.py
+++ core/migrations/0009_eventattendance_core_eventa_event_i_79d85f_idx.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_auto_20251023_0025'),
+        ('core', '0007_eventattendance_delete_eventvote_and_more'),
     ]
 
     operations = [
